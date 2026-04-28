0277b81eca8077fbe3acd8ea7adb33beeea3a66a
kalkidan <kalukassahun29@gmail.com>
2026-04-28 12:42:20 +0300

    fix: add logging instead of using just print()

--- src/data_processor.py
+++ src/data_processor.py
@@ -3,47 +3,60 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
+import logging
 
 from scipy import stats
 import os
 
-
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
 class Country_Eda:
     def __init__(self, name):
        self.name=name
        self.df=None
         
     def load_data(self):
-        current_dir = os.path.dirname(os.path.abspath(__file__))
-    
-    # 2. Move up one level to the root, then into the 'csv' folder
-    # This creates /mount/src/climate-challenge-week0/csv/ethiopia.csv
-        file_path = os.path.join(current_dir, '..', 'csv', f'{self.name}.csv')
-    
-    # 3. Standardize the path for the operating system
-        file_path = os.path.normpath(file_path)
-    
-        if not os.path.exists(file_path):
-            raise FileNotFoundError(f"Could not find the file at: {file_path}")
+        """Loads data with error handling for missing files."""
+        try:
+            current_dir = os.path.dirname(os.path.abspath(__file__))
+            file_path = os.path.join(current_dir, '..', 'csv', f'{self.name}.csv')
 
-        self.df = pd.read_csv(file_path)
-        print(f'_____ Data Loaded Successfully from {file_path} ________\n\n')
-        print('_____ Data Loaded Successfully________\n\n')
+            file_path = os.path.normpath(file_path)
+    
+            if not os.path.exists(file_path):
+                raise FileNotFoundError(f"Could not find the file at: {file_path}")
+
+            self.df = pd.read_csv(file_path)
+            logger.info(f"Successfully loaded data for {self.name}")
+        except Exception as e:
+            logger.error(f"Failed to load data: {e}")
+            raise
 
 
 
     def specific_country(self):
         self.df['COUNTRY']=self.name.capitalize()
-        print('_________Specific country set________\n\n')
+        logger.info('Specific country set')
 
     def date_parser(self):
-        self.df['DATE']=pd.to_datetime(
-        self.df['YEAR'].astype(str) + ' ' + self.df['DOY'].astype(str), 
-        format='%Y %j')
-        self.df['MONTH']=self.df['DATE'].dt.month
-        print('________date parsed successfully & month extracted succesfully______\n\n')
+        """Parses dates with validation."""
+        try:
+            if 'YEAR' not in self.df.columns or 'DOY' not in self.df.columns:
+                raise KeyError("Missing YEAR or DOY columns for date parsing.")
+            
+            self.df['DATE']=pd.to_datetime(
+            self.df['YEAR'].astype(str) + ' ' + self.df['DOY'].astype(str), format='%Y %j')
+
+            self.df['MONTH']=self.df['DATE'].dt.month
+
+            logger.info('date parsed successfully & month extracted succesfully')
+        except Exception as e:
+            logger.error(f"Error in date_parser: {e}")
+            raise
+            
     
     def data_quality(self):
+        """Checks quality and handles nulls systematically."""
         print(f'--------------Data Quality Report for {self.name.capitalize()}-----------')
         print("\n--- Data Information ---\n")
         print(self.df.info())
@@ -51,11 +64,11 @@ class Country_Eda:
         dup_count=self.df.duplicated().sum()
         if dup_count > 0:
             self.df=self.df.drop_duplicates()
-            print(f'Found and dropped {dup_count} duplicates')
+            logger.info(f'Found and dropped {dup_count} duplicates')
         else:
-            print('Duplicates: no duplicates found')
+            logger.info('Duplicates: no duplicates found')
         self.df=self.df.replace(-999, np.nan)
-        print('Replaced all -999 occurrences with NaN.')
+        logger.info('Replaced all -999 occurrences with NaN.')
         
         print('\n-------------- missing data check ---------------\n')
        
@@ -67,11 +80,11 @@ class Country_Eda:
         too_nulls = null_perc[null_perc > 5]
         if not too_nulls.empty:
             for col, val in too_nulls.items():
-                print(f"aolumn '{col}' has {val:.2f}% missing data. this may affect seasonal analysis.")
+                logger.warning(f"aolumn '{col}' has {val:.2f}% missing data. this may affect seasonal analysis.")
         else:
-            print("all columns have < 5% missing data. good for analysis.")
+            logger.info("all columns have < 5% missing data. good for analysis.")
         
-        print("\n--- Summary Statistics ---\n")
+        logger.info("\n--- Summary Statistics ---\n")
         stats_summary = self.df.describe()
         print(stats_summary) 
 
@@ -79,6 +92,7 @@ class Country_Eda:
    
 
     def check_outliers(self, threshold=3):
+        """ Checks for outliers and reasons for decisions. """
         cols=['T2M', 'T2M_MAX', 'T2M_MIN', 'PRECTOTCORR', 'RH2M', 'WS2M', 'WS2M_MAX']
         z_score=self.df[cols].apply(stats.zscore)
         norm_z_score=np.abs(z_score)
@@ -87,7 +101,7 @@ class Country_Eda:
         outliers=self.df[outlier_indices]
         perc=(len(outliers)/len(self.df)) * 100
         
-        print('------------ Outliers Report-------------------\n')
+        logger.info('------------ Outliers Report-------------------\n')
         if perc < 1:
             reasoning = "Clean Data: Minimal outliers detected. Retaining all points."
         elif perc < 7:
@@ -95,25 +109,26 @@ class Country_Eda:
         else:
             reasoning = "High Volatility: Significant outliers detected. Suggests sensor noise or extreme climate shifts. Decision: INSPECT/CAP."
 
-        print(f'total rows flagged as outliers {len(outliers)} and the percentage is {perc}%\n')  
+        logger.warning(f'total rows flagged as outliers {len(outliers)} and the percentage is {perc}%\n')  
 
-        print(f"REASONING: {reasoning}\n")
+        logger.info(f"REASONING: {reasoning}\n")
         print('------------- Missing Value Handling---------------')
         initial=len(self.df)
         min_threshold = int(len(self.df.columns) * 0.7)
         self.df=self.df.dropna(thresh=min_threshold)
         dropped=initial - len(self.df)
         if dropped > 0:
-            print(f"dropped {dropped} rows because they were more than 30% empty.")
+            logger.warning(f"dropped {dropped} rows because they were more than 30% empty.")
         
         self.df[cols] = self.df[cols].ffill()
-        print("applied ffill to remaining missing weather variables.")
+        logger.info("applied ffill to remaining missing weather variables.")
         print('-------------------------------------------------\n')
 
 
 
     def plotting_average_monthly_T2M(self):
-        print('________Preparing resampled Monthly data for T2M________\n')
+        """ Plot average monthly T2M"""
+        logger.info('________Preparing resampled Monthly data for T2M________\n')
         
     
         monthly_df = self.df.set_index('DATE')['T2M'].resample('MS').mean().reset_index()
@@ -155,28 +170,29 @@ class Country_Eda:
         plt.show()
 
        
-        print(f"\n--- Automated Insights for {self.name.capitalize()} ---")
+        logger.info(f"\n--- Automated Insights for {self.name.capitalize()} ---")
         
         
         trend_desc = "increasing (warming)" if slope > 0 else "decreasing (cooling)"
-        print(f"1. TREND: The temperature is generally {trend_desc} at a rate of {slope*12:.3f}°C per year.")
+        logger.info(f"1. TREND: The temperature is generally {trend_desc} at a rate of {slope*12:.3f}°C per year.")
         
         
-        print(f"2. VOLATILITY: The total temperature swing across the period is {temp_range:.1f}°C.")
+        logger.info(f"2. VOLATILITY: The total temperature swing across the period is {temp_range:.1f}°C.")
         if temp_range > 15:
-            print("   Note: High seasonal variance detected (likely a high-altitude or inland climate).")
+            logger.info("   Note: High seasonal variance detected (likely a high-altitude or inland climate).")
         
         
         if abs(recent_avg - historic_avg) > 1.0:
             status = "warmer" if recent_avg > historic_avg else "cooler"
-            print(f"3. ANOMALY: The most recent 12 months are {abs(recent_avg - historic_avg):.2f}°C {status} than the first 12 months.")
+            logger.info(f"3. ANOMALY: The most recent 12 months are {abs(recent_avg - historic_avg):.2f}°C {status} than the first 12 months.")
         else:
-            print("3. ANOMALY: No significant long-term shift detected between early and late periods.")
+            logger.info("3. ANOMALY: No significant long-term shift detected between early and late periods.")
         print("---------------------------------------------------\n")
 
 
 
     def monthly_total_precipitation(self):
+        """ Plot monthly total precipitation."""
         
         monthly=self.df.groupby('MONTH')['PRECTOTCORR'].sum().reset_index()
         month_map = {
@@ -206,6 +222,7 @@ class Country_Eda:
         
 
     def correlation_and_relationship(self):
+        """Checks and plots correlations and relationships"""
         fig, ax= plt.subplots(figsize=(10,6))
         corr=self.df.corr(numeric_only=True)
         sns.heatmap(data=corr, annot=True, cmap='coolwarm', ax=ax)
@@ -213,11 +230,11 @@ class Country_Eda:
         corr_m=corr.unstack()
         sorted_corr=corr_m[corr_m < 1].sort_values(ascending=False, key=abs)
         top_3 = sorted_corr.head(6)[::2]
-        print(f"\n--- Top 3 Strongest Relationships for {self.name.capitalize()} ---")
+        logger.info(f"\n--- Top 3 Strongest Relationships for {self.name.capitalize()} ---")
         for i, ((var1, var2), val) in enumerate(top_3.items(), 1):
             strength = "Strong" if abs(val) > 0.7 else "Moderate"
             direction = "Positive" if val > 0 else "Negative"
-            print(f"{i}. {var1} & {var2}: {val:.2f} ({strength} {direction})")
+            logger.info(f"{i}. {var1} & {var2}: {val:.2f} ({strength} {direction})")
         print("-----------------------------------------------------------\n")
 
 
@@ -234,26 +251,27 @@ class Country_Eda:
     
 
     def distribution_analysis(self):
+        """ check and analyse the distribution of PRECTOTCORR"""
         skew=self.df['PRECTOTCORR'].skew()
         fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(16, 6))
         sns.histplot(self.df['PRECTOTCORR'], kde=True, ax=ax1, color='blue')
         ax1.set_title(f'Original Distribution (Skew: {skew:.2f})')
         
         if skew > 1:
-            print('The distribution of total daily precipitation is strongly right-skewed. the shape would be long right tail, bulk on the left')
+            logger.info('The distribution of total daily precipitation is strongly right-skewed. the shape would be long right tail, bulk on the left')
             log_data = np.log1p(self.df['PRECTOTCORR'])
             
             sns.histplot(log_data, kde=True, ax=ax2, color='green')
             ax2.set_title('Log-Transformed Distribution (Balanced)')
             ax2.set_xlabel('Log(Precipitation + 1)')
         elif skew < -1:
-            print('The distribution of total daily precipitation is strongly left-skewed. the shape would be bulk on the right, tail to the left')
+            logger.info('The distribution of total daily precipitation is strongly left-skewed. the shape would be bulk on the right, tail to the left')
             ax2.axis('off')
         elif -0.5 < skew < 0.5:
-            print('the distribution of total daily precipitation is fairly symmetric, Bell curve')
+            logger.info('the distribution of total daily precipitation is fairly symmetric, Bell curve')
             ax2.axis('off')
         else:
-            print('The distribution of total daily precipitation is symmetric and normal. the shape would be bell-shaped, tails similar')
+            logger.info('The distribution of total daily precipitation is symmetric and normal. the shape would be bell-shaped, tails similar')
             ax2.axis('off')
         
         # bubble
@@ -278,7 +296,7 @@ class Country_Eda:
         
         file_path = os.path.join(target_dir, f'{self.name}_clean.csv')
         self.df.to_csv(file_path, index=False, encoding='utf-8')
-        print('______exported successfully_______')
+        logger.info('______exported successfully_______')
 
 
 
